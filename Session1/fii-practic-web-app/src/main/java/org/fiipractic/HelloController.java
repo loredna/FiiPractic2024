8f27cea12aba06333b801246b3cc427754e1f4b5
loredana_surugiu <loredana_surugiu@yahoo.com>
2024-03-11 19:52:48 +0200

    Add presentation and applications of the first session

---
+++ Session1/fii-practic-web-app/src/main/java/org/fiipractic/HelloController.java
@@ -0,0 +1,17 @@
+package org.fiipractic;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloController extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.getOutputStream().println("Hello from Fii Practic!");
+    }
+}
---
+++ Session1/fii-practic-web-app/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Welcome, this is default page!</h2>
+</body>
+</html>
